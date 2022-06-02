d62555606142d2461a293ad4bd3d0d92c8d55a8e
Roman Zaycev <box@romanzaycev.ru>
2022-06-03 00:48:51 +0700

    Experiments

--- MTQtoolEditor/ViewModels/MainWindowViewModel.cs
+++ MTQtoolEditor/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,27 @@
+using ReactiveUI;
+
 namespace MTQtoolEditor.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Hello world!";
+    private static readonly string[] Greetings = new string[2] {
+        "Hello world!",
+        "YOLO"
+    };
+
+    private string _greeting = Greetings[0];
+    public string Greeting
+    {
+        get => _greeting;
+        set => this.RaiseAndSetIfChanged(ref _greeting, value);
+    }
+
+    public MainWindowViewModel()
+    {
+    }
+
+    public void OnToggleTextCommand()
+    {
+        Greeting = _greeting == Greetings[0] ? Greetings[1] : Greetings[0];
+    }
 }
